3e69265dbf0e97c300131056152c64805d02d9ed
Irene Woo <irenewoo@Irenes-MacBook-Air.local>
2017-10-21 17:12:02 -0400

    stuff

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+    name: "Project",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2)
+    ]
+)
